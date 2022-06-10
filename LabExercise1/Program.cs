da5b1e2177b343aa476308f3f45ecac356a546ef
Tontan-Hak <tontanhak97@gmail.com>
2022-06-10 17:52:05 +0700

    Add project files.

---
+++ LabExercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExercise1
+{
+    class Car
+    {
+        public string strModel;
+        public string strBrand;
+        public float fltPrice;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car objCar1;         //part (b) declare an object name of type car
+            objCar1 = new Car(); // Part (c) instantiate the object objCar1
+            // Car objCar1 = new Car();
+
+            Console.Write("What is the model of your car: ");
+            objCar1.strModel = Console.ReadLine();
+            Console.Write("What is the brand of your car: ");
+            objCar1.strBrand = Console.ReadLine();
+            Console.Write("What is the price of your car: ");
+            objCar1.fltPrice = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Car Model {0}, Brand is {1}, price is {2}", objCar1.strModel, objCar1.strBrand, objCar1.fltPrice);
+        }
+    }
+}
---
+++ Lesson01ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01ConsoleApp
+{
+    class student
+    {
+        public string strName;
+        public string strCourse;
+        public string strGroup;
+        public int intAge;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student obj1, obj2;
+            obj1 = new student();
+            obj2 = new student();
+            Console.Write("Enter the name for student 1: ");
+            obj1.strName = Console.ReadLine();
+            Console.Write("Enter the course name for student 2: ");
+            obj1.strCourse = (Console.WriteLine());
+            Console.Write("Enter the group name for student 1: ");
+            obj1.strGroup = Console.WriteLine();
+            Console.Write("Enter the age name for student 1: ");
+            obj1.intAge = int.Parse(Console.WriteLine());
+
+            Console.WriteLine("Your name is {0}, and you reply your age as {1}, and your Height as {2}", UserName, Age.ToString(), Height);
+        }
+    }
+}
