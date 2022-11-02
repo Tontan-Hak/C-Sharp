1ada7e2d7f81d540e31aa3fa8fa8f1250d06639e
Tontan Hak <tontanhak97@gmail.com>
2022-11-02 13:55:06 +0700

    Add files via upload

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02Example
+{
+    class House
+    {
+        public string strType;
+        private string strAddress;
+        static void Main(string[] args)
+        {
+            int x;
+            void getuserinput()
+
+        }
+    }
+}
